687f1e216b973940bcee2c048d80c2518615387f
ray-pH <rayhan.azizi9@gmail.com>
2024-12-23 03:59:54 +0700

    2024/d22/2 improve2

--- AOC2024/src/d22.rs
+++ AOC2024/src/d22.rs
@@ -30,23 +30,27 @@ const fn step(sec: i32) -> i32 {
 type ChangeTuple = (i8, i8, i8, i8);
 fn update_price_map(init_sec: i32, price_map: &mut HashMap<ChangeTuple, u32>) {
     let mut visited: HashSet<ChangeTuple> = HashSet::new();
-    let mut s0 = init_sec;
-    let mut s1 = step(s0);
-    let mut s2 = step(s1);
-    let mut s3 = step(s2);
+    let s0 = init_sec;
+    let s1 = step(s0);
+    let s2 = step(s1);
+    let s3 = step(s2);
     let mut s4 = step(s3);
+    let (d0, d1, d2, d3, mut d4) = (s0 % 10, s1 % 10, s2 % 10, s3 % 10, s4 % 10);
+    let mut c1 = (d1 - d0) as i8;
+    let mut c2 = (d2 - d1) as i8;
+    let mut c3 = (d3 - d2) as i8;
+    let mut c4 = (d4 - d3) as i8;
     for _ in 4..2000 {
-        let (d0, d1, d2, d3, d4) = (s0 % 10, s1 % 10, s2 % 10, s3 % 10, s4 % 10);
-        let c1 = d1 - d0;
-        let c2 = d2 - d1;
-        let c3 = d3 - d2;
-        let c4 = d4 - d3;
-        let key = (c1 as i8, c2 as i8, c3 as i8, c4 as i8);
-        if !visited.contains(&key) {
-            visited.insert(key);
-            price_map.entry(key).or_default().add_assign(d4 as u32);
+        if d4 > 1 {
+            let key = (c1, c2, c3, c4);
+            if !visited.contains(&key) {
+                visited.insert(key);
+                price_map.entry(key).or_default().add_assign(d4 as u32);
+            }
         }
-        (s0, s1, s2, s3, s4) = (s1, s2, s3, s4, step(s4));
+        s4 = step(s4);
+        (c1, c2, c3, c4) = (c2, c3, c4, (s4 % 10 - d4) as i8);
+        d4 = s4 % 10;
     }
 }
 
