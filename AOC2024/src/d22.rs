c029e383ead5f384a449f5496690e953cc7f4612
ray-pH <rayhan.azizi9@gmail.com>
2024-12-23 05:00:25 +0700

    2024/d22 clippy

--- AOC2024/src/d22.rs
+++ AOC2024/src/d22.rs
@@ -38,11 +38,9 @@ fn update_price_map(init_sec: i32, price_map: &mut HashMap<u32, u32>) {
     let (c1, c2, c3, c4) = ((d1 - d0) as u8, (d2 - d1) as u8, (d3 - d2) as u8, (d4 - d3) as u8);
     let mut key: u32 = (c1 as u32) << 24 | (c2 as u32) << 16 | (c3 as u32) << 8 | (c4 as u32);
     for _ in 4..2000 {
-        if d4 > 1 {
-            if !visited.contains(&key) {
-                visited.insert(key);
-                price_map.entry(key).or_default().add_assign(d4 as u32);
-            }
+        if d4 > 1 && !visited.contains(&key) {
+            visited.insert(key);
+            price_map.entry(key).or_default().add_assign(d4 as u32);
         }
         s4 = step(s4);
         key = ( key & 0xFF_FF_FF ) << 8 | (( s4 % 10 - d4 ) as u8) as u32;
