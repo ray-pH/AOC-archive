261f9b096d27ab15bb664884d3d8775ec48ddd7f
ray-pH <rayhan.azizi9@gmail.com>
2024-12-23 04:35:38 +0700

    2024/d22/2 improve3

--- AOC2024/src/d22.rs
+++ AOC2024/src/d22.rs
@@ -6,7 +6,7 @@ pub fn part1(input: &str) -> String {
 }
 pub fn part2(input: &str) -> String {
     let nums = parse_input(input);
-    let mut price_map: HashMap<ChangeTuple, u32> = HashMap::new();
+    let mut price_map = HashMap::new();
     nums.iter().for_each(|n| update_price_map(*n, &mut price_map));
     price_map.values().max().unwrap().to_string()
 }
@@ -27,29 +27,25 @@ const fn step(sec: i32) -> i32 {
     sec
 }
 
-type ChangeTuple = (i8, i8, i8, i8);
-fn update_price_map(init_sec: i32, price_map: &mut HashMap<ChangeTuple, u32>) {
-    let mut visited: HashSet<ChangeTuple> = HashSet::new();
+fn update_price_map(init_sec: i32, price_map: &mut HashMap<u32, u32>) {
+    let mut visited: HashSet<u32> = HashSet::new();
     let s0 = init_sec;
     let s1 = step(s0);
     let s2 = step(s1);
     let s3 = step(s2);
     let mut s4 = step(s3);
     let (d0, d1, d2, d3, mut d4) = (s0 % 10, s1 % 10, s2 % 10, s3 % 10, s4 % 10);
-    let mut c1 = (d1 - d0) as i8;
-    let mut c2 = (d2 - d1) as i8;
-    let mut c3 = (d3 - d2) as i8;
-    let mut c4 = (d4 - d3) as i8;
+    let (c1, c2, c3, c4) = ((d1 - d0) as u8, (d2 - d1) as u8, (d3 - d2) as u8, (d4 - d3) as u8);
+    let mut key: u32 = (c1 as u32) << 24 | (c2 as u32) << 16 | (c3 as u32) << 8 | (c4 as u32);
     for _ in 4..2000 {
         if d4 > 1 {
-            let key = (c1, c2, c3, c4);
             if !visited.contains(&key) {
                 visited.insert(key);
                 price_map.entry(key).or_default().add_assign(d4 as u32);
             }
         }
         s4 = step(s4);
-        (c1, c2, c3, c4) = (c2, c3, c4, (s4 % 10 - d4) as i8);
+        key = ( key & 0xFF_FF_FF ) << 8 | (( s4 % 10 - d4 ) as u8) as u32;
         d4 = s4 % 10;
     }
 }
